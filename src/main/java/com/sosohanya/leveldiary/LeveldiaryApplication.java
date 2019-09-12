e11e97c9029782dd0584329a8cf584ce3a4cc3bb
ujuin81 <ujuin81@gmail.com>
2019-09-12 15:41:49 +0900

    project init

---
+++ src/main/java/com/sosohanya/leveldiary/LeveldiaryApplication.java
@@ -0,0 +1,13 @@
+package com.sosohanya.leveldiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeveldiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeveldiaryApplication.class, args);
+	}
+
+}
